42d8062ee2bd9f33369b32fa1abc0e13b8a21ee4
Back-Nova <abansantiago2.0@gmail.com>
2025-10-13 10:58:40 -0300

    proyecto

---
+++ src/main/java/com/cineclub/cineclub/CineclubApplication.java
@@ -0,0 +1,13 @@
+package com.cineclub.cineclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineclub/cineclub/CineclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineclub.cineclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
